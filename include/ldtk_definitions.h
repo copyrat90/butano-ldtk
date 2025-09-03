14aec1604121fc5f41a60eea75a617f883a13313
copyrat90 <copyrat90@gmail.com>
2025-09-03 11:04:30 +0900

    Add lookup functions in `ldtk::definitions`

--- include/ldtk_definitions.h
+++ include/ldtk_definitions.h
@@ -5,6 +5,8 @@
 #include "ldtk_layer_definition.h"
 #include "ldtk_tileset_definition.h"
 
+#include "ldtk_gen_idents_fwd.h"
+
 #include <bn_span.h>
 
 namespace ldtk
@@ -19,6 +21,56 @@ public:
                           const bn::span<const tileset_definition>& tilesets)
         : _entities(entities), _layers(layers), _level_fields(level_fields), _tilesets(tilesets)
     {
+    }
+
+public:
+    /// @brief Looks up a entity definition with its identifier.
+    /// @note Look-up is done via indexing, thus it's O(1).
+    /// @param identifier Unique identifier of the entity to search for.
+    /// @return Reference to the entity definition.
+    [[nodiscard]] constexpr auto get_entity_def(gen::entity_ident identifier) const -> const entity_definition&
+    {
+        BN_ASSERT((int)identifier >= 0, "Invalid identifier (gen::entity_ident)", (int)identifier);
+        BN_ASSERT((int)identifier < _entities.size(), "Out of bound identifier (gen::entity_ident)", (int)identifier);
+
+        return _entities.data()[(int)identifier];
+    }
+
+    /// @brief Looks up a layer definition with its identifier.
+    /// @note Look-up is done via indexing, thus it's O(1).
+    /// @param identifier Unique identifier of the layer to search for.
+    /// @return Reference to the layer definition.
+    [[nodiscard]] constexpr auto get_layer_def(gen::layer_ident identifier) const -> const layer_definition&
+    {
+        BN_ASSERT((int)identifier >= 0, "Invalid identifier (gen::layer_ident)", (int)identifier);
+        BN_ASSERT((int)identifier < _layers.size(), "Out of bound identifier (gen::layer_ident)", (int)identifier);
+
+        return _layers.data()[(int)identifier];
+    }
+
+    /// @brief Looks up a level field definition with its identifier.
+    /// @note Look-up is done via indexing, thus it's O(1).
+    /// @param identifier Unique identifier of the level field to search for.
+    /// @return Reference to the level field definition.
+    [[nodiscard]] constexpr auto get_level_field_def(gen::level_field_ident identifier) const -> const field_definition&
+    {
+        BN_ASSERT((int)identifier >= 0, "Invalid identifier (gen::level_field_ident)", (int)identifier);
+        BN_ASSERT((int)identifier < _level_fields.size(), "Out of bound identifier (gen::level_field_ident)",
+                  (int)identifier);
+
+        return _level_fields.data()[(int)identifier];
+    }
+
+    /// @brief Looks up a tileset definition with its identifier.
+    /// @note Look-up is done via indexing, thus it's O(1).
+    /// @param identifier Unique identifier of the tileset to search for.
+    /// @return Reference to the tileset definition.
+    [[nodiscard]] constexpr auto get_tileset_def(gen::tileset_ident identifier) const -> const tileset_definition&
+    {
+        BN_ASSERT((int)identifier >= 0, "Invalid identifier (gen::tileset_ident)", (int)identifier);
+        BN_ASSERT((int)identifier < _tilesets.size(), "Out of bound identifier (gen::tileset_ident)", (int)identifier);
+
+        return _tilesets.data()[(int)identifier];
     }
 
 public:
