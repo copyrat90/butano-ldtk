772c2ac039b84b79f326cf0f71aa8a627ee5a6e8
copyrat90 <copyrat90@gmail.com>
2025-11-14 08:49:22 +0900

    Use `bn::unique_ptr` in example for Butano 20.3.0

--- examples/typical_2d_platformer/include/player_state_ptr.h
+++ examples/typical_2d_platformer/include/player_state_ptr.h
@@ -9,12 +9,8 @@
 
 #include <bn_generic_pool.h>
 
-#include <memory>
-
 namespace t2p
 {
-
-class player_state_base;
 
 using player_states_pool_t = bn::generic_pool<MAX_PLAYER_STATE_SIZE, 3>;
 
@@ -30,6 +26,11 @@ private:
     player_states_pool_t* _pool = nullptr;
 };
 
+#if BN_VERSION_MAJOR > 20 || (BN_VERSION_MAJOR == 20 && BN_VERSION_MINOR >= 3)
+// Custom deleters for `bn::unique_ptr` are supported after Butano 20.3.0
+using player_state_ptr = bn::unique_ptr<player_state_base, player_state_deleter>;
+#else
 using player_state_ptr = std::unique_ptr<player_state_base, player_state_deleter>;
+#endif
 
 } // namespace t2p
--- examples/typical_2d_platformer/include/player_state_ptr_fwd.h
+++ examples/typical_2d_platformer/include/player_state_ptr_fwd.h
@@ -3,7 +3,13 @@
 
 #pragma once
 
+#include <bn_version.h>
+
+#if BN_VERSION_MAJOR > 20 || (BN_VERSION_MAJOR == 20 && BN_VERSION_MINOR >= 3)
+#include <bn_unique_ptr.h>
+#else
 #include <memory>
+#endif
 
 namespace t2p
 {
@@ -11,6 +17,11 @@ namespace t2p
 class player_state_base;
 class player_state_deleter;
 
+#if BN_VERSION_MAJOR > 20 || (BN_VERSION_MAJOR == 20 && BN_VERSION_MINOR >= 3)
+// Custom deleters for `bn::unique_ptr` are supported after Butano 20.3.0
+using player_state_ptr = bn::unique_ptr<player_state_base, player_state_deleter>;
+#else
 using player_state_ptr = std::unique_ptr<player_state_base, player_state_deleter>;
+#endif
 
 } // namespace t2p
